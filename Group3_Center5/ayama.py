f64f6eafc02127bb721d3addd0bb335bd6e394d3
stellalou0210 <stellalou0210@gmail.com>
2025-07-02 15:51:13 +0900

    Update ayama.py

--- Group3_Center5/ayama.py
+++ Group3_Center5/ayama.py
@@ -1,1 +1,10 @@
-print("5goukan")
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to 5goukan! 👋")
+
+st.sidebar.success("Select a demo above.")
