232499274c8c31b63ea81e9348fbcb5512118a4f
NasuShion <nasu.shion.144@s.kyushu-u.ac.jp>
2025-07-02 16:18:15 +0900

    Update shion.py

--- Group3_Center5/shion.py
+++ Group3_Center5/shion.py
@@ -1,3 +1,15 @@
-pip install streamlit
-streamlit hello
+import streamlit as st
 
+st.title("Hello Streamlit-er 👋")
+st.markdown(
+    """ 
+    This is the intro page to our website for Center 5 Ito Campus for students to have fun. 
+
+    **There's :rainbow[so much] you can build!**
+    
+    """
+)
+
+if st.button("Send balloons!"):
+    st.balloons()
+  
