027fb941289b5bbb47c590f44fb3c55fe91e1bc4
yyh327 <148839516+yyh327@users.noreply.github.com>
2025-07-09 15:52:59 +0900

    layout

--- Group3_Center5/Main.py
+++ Group3_Center5/Main.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
-st.title("Hello Streamlit-er 👋")
+st.title("Hello Fellow Students 👋")
 st.markdown(
     """ 
-    This is the intro page to our website for Center 5 Ito Campus for students to have fun. 
+    This is the intro page to our website for Center 5 Ito Campus for students to check the availability of each classrooms.
+
+    **👈 Select a tab from the dropdown on the left** to start exploring Center 5.
 
     **There's :rainbow[so much] you can explore!**
      
