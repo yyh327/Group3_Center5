d76a09dac8ca2f1906a5d0c7bb56b22c18e44f25
yyh327 <148839516+yyh327@users.noreply.github.com>
2025-07-09 15:24:12 +0900

    Update pages1_calendar.py
    calendar edit

--- Group3_Center5/pages/pages1_calendar.py
+++ Group3_Center5/pages/pages1_calendar.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import streamlit_calendar as st_calendar
-import numpy as np
 
 st.set_page_config(page_title="Calendar")
+
+st.markdown("Calendar")
+st.sidebar.header("Calendar")
 
 st_calendar.calendar()
