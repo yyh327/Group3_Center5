4f266be699edea536554613eb7502dabfa676fed
yyh327 <148839516+yyh327@users.noreply.github.com>
2025-07-09 15:44:08 +0900

    change
    change to main page file

--- Group3_Center5/Main.py
+++ Group3_Center5/Main.py
@@ -5,7 +5,7 @@ st.markdown(
     """ 
     This is the intro page to our website for Center 5 Ito Campus for students to have fun. 
 
-    **There's :rainbow[so much] you can build!**
+    **There's :rainbow[so much] you can explore!**
      
     """
 )
